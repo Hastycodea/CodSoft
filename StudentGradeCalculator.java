8dad152e479f9e6bd0dfbfe1d6247fc41464fea8
unknown <hastycodeandreh@gmail.com>
2024-06-15 12:39:38 +0300

    inputMarks

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StudentGradeCalculator {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> subjectOne = new ArrrayList()<>;
+
+        System.out.println("Enter marks for subject one: ");
+        String subjectOne = scanner.nextLine();
+
+
+        
+    }    
 }
