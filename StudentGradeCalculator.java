47fe23fa4389378a7d81f77537caaf1ea1bb5fcf
unknown <hastycodeandreh@gmail.com>
2024-06-15 23:07:16 +0300

    results

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -5,12 +5,41 @@ public class StudentGradeCalculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> subjectOne = new ArrrayList()<>;
+        ArrayList<String> subjects = new ArrayList<String>();
 
-        System.out.println("Enter marks for subject one: ");
-        String subjectOne = scanner.nextLine();
+        int i = 0;
 
+        while (i < 5) {
+            System.out.print("Enter marks : ");
+            String marks = scanner.nextLine(); 
+            int results = Integer.valueOf(marks);
+            if (results > 100 || results < 0) {
+                System.out.println("Invalid marks!");
+            } else {
+                subjects.add(marks);
+            }
 
-        
-    }    
+            i++;
+        }
+
+        int sum = 0;
+        for (String subject : subjects) {
+            int values = Integer.valueOf(subject);
+            sum += values;
+        }
+        double percentage = sum / 5;
+        System.out.println("Total Marks: " + sum);
+        System.out.println("Average Percentage: " + percentage + " %");
+
+        if (percentage >= 70) {
+            System.out.println("Grade: A");            
+        } else if (percentage >= 60) {
+            System.out.println("Grade: B");
+        } else if (percentage >= 50) {
+            System.out.println("Grade: C");
+        } else {
+            System.out.println("Grade: D");
+        }
+
+    }
 }
