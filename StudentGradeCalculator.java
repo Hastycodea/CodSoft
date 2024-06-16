fa8e762b2f05a80a14638d6ec11dc12e7e4228cc
unknown <hastycodeandreh@gmail.com>
2024-06-16 22:26:15 +0300

    subjectNo + quit

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -7,11 +7,17 @@ public class StudentGradeCalculator {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> subjects = new ArrayList<String>();
 
-        int i = 0;
+        int i = 1;
+        int total = 0;
+        while (true) {
+            System.out.print("Subject " + i + " marks (quit to exit): ");
+            String marks = scanner.nextLine();
+            total++;
 
-        while (i < 5) {
-            System.out.print("Enter marks : ");
-            String marks = scanner.nextLine(); 
+            if (marks.equals("quit")) {
+                break;                
+            }
+
             int results = Integer.valueOf(marks);
             if (results > 100 || results < 0) {
                 System.out.println("Invalid marks!");
@@ -27,7 +33,7 @@ public class StudentGradeCalculator {
             int values = Integer.valueOf(subject);
             sum += values;
         }
-        double percentage = sum / 5;
+        double percentage = sum / total;
         System.out.println("Total Marks: " + sum);
         System.out.println("Average Percentage: " + percentage + " %");
 
