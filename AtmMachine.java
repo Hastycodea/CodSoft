9d5ed0ffd711e63bdd008dccc0c87ef6ecb07984
unknown <hastycodeandreh@gmail.com>
2024-06-17 22:05:52 +0300

    choices

--- AtmMachine.java
+++ AtmMachine.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 
 public class AtmMachine {
-    public static void main(String[] args) {
-        
+
+    private UserBankAccount account;
+
+    public AtmMachine(UserBankAccount account) {
+        this.account = account;
     }
 
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
