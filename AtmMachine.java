9beb4aae1db594b9e1ccb2250a5df8ce30e842ea
unknown <hastycodeandreh@gmail.com>
2024-06-17 21:44:22 +0300

    updates

---
+++ AtmMachine.java
@@ -0,0 +1,7 @@
+
+public class AtmMachine {
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ UserBankAccount.java
@@ -0,0 +1,9 @@
+public class UserBankAccount {
+    private double balance;
+
+    public UserBankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    
+}
