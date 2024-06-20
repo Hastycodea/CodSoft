5cc278f3f526c8b0f19b6b2901b7bf84bc5c3e08
unknown <hastycodeandreh@gmail.com>
2024-06-20 12:13:42 +0300

    userInput

---
+++ CurrencyConverter.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CurrencyConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Currency selection
+        System.out.print("Choose the base currency: ");
+        String baseCurrency = scanner.nextLine();
+
+        System.out.print("Choose the target currency: ");
+        String targetCurrency = scanner.nextLine();
+        
+    }
+}
